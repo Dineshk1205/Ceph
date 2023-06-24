bc37b2ddcd41cde1d4674d5f69b24aae6009406d
Dineshk1205 <93421112+Dineshk1205@users.noreply.github.com>
2023-06-24 21:18:19 +0530

    Add files via upload

---
+++ Ceph.sh
@@ -0,0 +1,33 @@
+#Run on Ceph0
+
+cephadm bootstrap --mon-ip 172.90.0.30 --initial-dashboard-user "ceph-admin" --initial-dashboard-password "ADMIN_123" --dashboard-password-noupdate --cluster-network=172.91.0.0/24 --allow-fqdn-hostname
+
+
+ssh-copy-id -f -i /etc/ceph/ceph.pub root@ceph1
+ssh-copy-id -f -i /etc/ceph/ceph.pub root@ceph2
+
+
+ceph orch host add ceph1.kdinesh.in 172.90.0.31
+ceph orch host add ceph2.kdinesh.in 172.90.0.32
+
+
+ceph orch apply mon --placement="ceph0.kdinesh.in,ceph1.kdinesh.in,ceph2.kdinesh.in"
+ceph orch apply mgr --placement="ceph0.kdinesh.in,ceph1.kdinesh.in,ceph2.kdinesh.in"
+
+
+ceph orch host label add ceph0.kdinesh.in osd-node
+ceph orch host label add ceph1.kdinesh.in osd-node
+ceph orch host label add ceph2.kdinesh.in osd-node
+
+
+ceph orch host label add ceph0.kdinesh.in mon
+ceph orch host label add ceph1.kdinesh.in mon
+ceph orch host label add ceph2.kdinesh.in mon
+
+
+ceph orch host label add ceph0.kdinesh.in mgr
+ceph orch host label add ceph1.kdinesh.in mgr
+ceph orch host label add ceph2.kdinesh.in mgr
+
+
+ceph orch apply osd --all-available-devices
---
+++ cephpreq.sh
@@ -0,0 +1,17 @@
+#Run on All nodes
+
+#NTP Configuration --
+
+sed 's/^pool/#&/' -i /etc/chrony.conf
+echo -e "pool 172.90.0.30  iburst \nallow 172.90.0.0/24 " >> /etc/chrony.conf
+systemctl enable chronyd
+systemctl restart chronyd
+
+
+#Ceph installation
+
+curl --silent --remote-name --location https://github.com/ceph/ceph/raw/quincy/src/cephadm/cephadm
+ chmod +x cephadm
+ ./cephadm add-repo --release quincy
+ ./cephadm install
+ ./cephadm install  ceph-common
